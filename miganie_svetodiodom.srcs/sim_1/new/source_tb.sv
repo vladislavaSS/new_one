f072b5e5fb372e53d26edd57c0ae17774df10760
VladislavaSS <166354868+vladislavaSS@users.noreply.github.com>
2024-04-27 15:45:35 +0700

    доделываю доп задания

--- miganie_svetodiodom.srcs/sim_1/new/source_tb.sv
+++ miganie_svetodiodom.srcs/sim_1/new/source_tb.sv
@@ -1,6 +1,7 @@
 `timescale 1ns / 1ps
 
  module source_tb#(
+  parameter C_MAX_DATA = 10,
   parameter G_BYT = 1,
   parameter W = 8*G_BYT
   );
@@ -10,6 +11,7 @@
  logic i_clk = '0;
  logic i_rst = '0;
  logic s_ready = '1;
+ logic q_ready = '1;
  int Length = 0;
  
   source #(
@@ -17,19 +19,19 @@
     UUT_2(
      .i_clk (i_clk),
      .i_rst (i_rst),
-     .s_ready (s_ready),
+     .s_ready (q_ready),
      .Length (Length)
      );
 
  always #(T_CLK) i_clk = ~i_clk;
  always #(T_CLK*10) s_ready = ~s_ready;
+ always_ff @(posedge i_clk) q_ready <= s_ready;
     
 
  initial begin
     i_rst = '1;
     #(T_CLK*15)
     i_rst = '0;
-    #(T_CLK*60);
     Length += 10;
     #(T_CLK*60);
     Length += 10;
--- miganie_svetodiodom.srcs/sources_1/new/source.sv
+++ miganie_svetodiodom.srcs/sources_1/new/source.sv
@@ -1,6 +1,8 @@
 `timescale 1ns / 1ps
 
 module source#(
+ parameter int G_MAX_DATA = 10,
+ parameter int G_CNT_WIDTH = $ceil($clog2(G_MAX_DATA + 1)),
  parameter G_BYT = 1,
  parameter W = 8*G_BYT)
  (
@@ -8,117 +10,114 @@ module source#(
     input i_clk,
     input s_ready,
     
-    input int Length, //for input length of data
-    input int Length_buff,
+    input [G_CNT_WIDTH - 1 : 0] Length, //for input length of data
     
     output logic s_valid = '0,
-    output logic [7:0] s_data = '0
+    output logic [W - 1 : 0] s_data = '0
     );
-    
-//     if (Length > 0) begin  
-//        assign Length_buff = Length; end
      
-    int C_MAX_DATA;
+    //int C_MAX_DATA;
     
-    //localparam int C_MAX_DATA = 10;
-    localparam int C_MAX_IDLE = 50;
+    int Length_buff;
+    
+    //localparam int C_MAX_DATA = 10; //  
+    localparam int C_MAX_IDLE = 20;
     localparam int C_MAX_PSE = 10;
     
-    int C_DATA = $ceil($clog2(C_MAX_DATA - 1)); //localparam
-    localparam int C_IDLE = $ceil($clog2(C_MAX_IDLE - 1));
-    localparam int C_PSE = $ceil($clog2(C_MAX_PSE - 1));
+    //localparam int C_DATA = $ceil($clog2(C_MAX_DATA + 1)); //localparam
+    localparam int C_IDLE = $ceil($clog2(C_MAX_IDLE + 1));
+    localparam int C_PSE = $ceil($clog2(C_MAX_PSE + 1));
     
-    logic [31 : 0] q_data_cnt = '0; //C_DATA - 1
-    logic [C_IDLE - 1 : 0] q_pse_cnt = '0;
-    logic [C_PSE - 1 : 0] q_idle_cnt = '0;
-    logic [7:0] m_data = '0; 
+    
+    logic [G_CNT_WIDTH - 1 : 0] q_data_cnt = '0;
+    //logic [C_DATA - 1 /*31*/ : 0] q_data_cnt = '0; //C_DATA - 1
+    logic [C_PSE - 1 : 0] q_pse_cnt = '0; 
+    logic [C_IDLE - 1 : 0] q_idle_cnt = '0;
+    logic [W - 1 : 0] m_data = '0; 
 
-    
 	typedef enum {
-      S0 = 0,
-      S1 = 1,
-      S2 = 2,
-      S3 = 3,
-      S4 = 4,
-      S5 = 5,
-      S6 = 6
+      S0 = 0,  // Ready
+      S1 = 1,  // Header    
+      S2 = 2,  // Length    
+      S3 = 3,  // Payload   
+      S4 = 4,  // CRC_PAUSE 
+      S5 = 5,  // CRC_DATA      
+      S6 = 6   // Idle      
     } signals;
     
     signals signal = S0;
-	
 	
 	
 	always_ff @(posedge i_clk) begin
 	if (i_rst) 
 	   signal <= S0;
 	else
-//	   if (Length < 0)//(m_data == s_data && s_valid == 1)   
-//            assign Length_buff = Length; 
        case (signal)
          S0:
             begin 
-//                if (m_data == s_data)
-//                    assign Length_buff = Length;
-                C_MAX_DATA <= Length_buff;
-                
                 signal = (s_ready) ? S1 : S0; 
                 s_valid <= '0;
-                q_data_cnt <= '0;
+//                q_data_cnt <= '0; 
+                q_data_cnt <=  1;//'0; 
                 q_pse_cnt  <= '0;
                 q_idle_cnt <= '0;
             end
          S1: 
             begin
                 signal  <= (s_valid && s_ready) ? S2 : S1;
+                if (Length_buff == 0)
+                    Length_buff <= Length;
+//                G_MAX_DATA <= Length_buff;
+                
                 s_valid <= !(s_valid && s_ready);
                 s_data  <= 72;
             end
          S2: 
-//            begin signal <= (s_ready && s_valid) ? S3 : S2; end
             begin
                 signal  <= (s_valid && s_ready) ? S3 : S2;
                 s_valid <= !(s_valid && s_ready);
-                s_data  <= (s_valid && s_ready) ? '0 : C_DATA;
+//                s_data  <= C_MAX_DATA;
+                s_data  <= (s_valid && s_ready) ? '0 : Length_buff;
             end
          S3:
             begin
-                signal  <= ((q_data_cnt == C_DATA - 1) && (s_valid && s_ready)) ? S4: S3; 
-                s_valid <= !((q_data_cnt == C_DATA - 1) && (s_valid && s_ready)); 
+//                signal  <= ((q_data_cnt == C_MAX_DATA - 1) && (s_valid && s_ready)) ? S4: S3; 
+//                s_valid <= !((q_data_cnt == C_MAX_DATA - 1) && (s_valid && s_ready)); 
+                signal  <= ((q_data_cnt == Length_buff) && (s_valid && s_ready)) ? S4: S3; 
+                s_valid <= !((q_data_cnt == Length_buff) && (s_valid && s_ready)); 
+                
+                /*s_data  <= 72; */
+                
+                /*if (s_valid && s_ready) begin
+                     s_data  <= (s_valid && s_ready) ? '0 : C_MAX_DATA; end */
+                    
+//                s_data <= q_data_cnt; //end
+                
                 if (s_valid && s_ready) begin
                     q_data_cnt <= q_data_cnt + 1;
-                    s_data <= q_data_cnt + 1;
-                end
+                    s_data <= q_data_cnt; end 
             end
          S4: 
-            begin signal <= S5; end //S6 change S5
-//            begin signal <= (q_pse_cnt == C_PSE - 1) ? S5 : S4; //S6 change S5
-//            q_pse_cnt <= (q_pse_cnt == C_MAX_PSE - 1) ? '0 : (q_pse_cnt + 1); end
+            begin signal <= S5;
+             end 
          S5: 
             begin signal = (s_valid && s_ready) ? S6 : S5;
-            s_valid <= !(s_valid && s_ready);
-            s_data  <= m_data; end
+                  s_valid <= !(s_valid && s_ready);
+                  s_data  <= m_data;
+            end
          S6: 
             begin 
-            if (m_data == s_data)
-                    assign Length_buff = Length;
+            signal = (C_MAX_IDLE - 1 == q_idle_cnt) ? S0 : S6; 
+            q_idle_cnt <= (q_idle_cnt == C_MAX_IDLE - 1) ? '0 : (q_idle_cnt + 1);
+    
+              if (m_data == s_data)
+                       Length_buff = Length;
+            end
             
-            signal = (C_IDLE - 1 == q_idle_cnt) ? S0 : S6; 
-            q_idle_cnt <= (q_idle_cnt == C_MAX_PSE - 1) ? '0 : (q_idle_cnt + 1);
-            
-                 end
          default: signal <= S0;
        
       endcase;
     end 
-   
-//   always_comb begin 
-//        if (signal == S4) begin q_pse_cnt = (q_pse_cnt == C_MAX_PSE - 1) ? '0 : (q_pse_cnt + 1); end
-//        if (signal == S6) begin q_pse_cnt = (q_idle_cnt == C_MAX_PSE - 1) ? '0 : (q_idle_cnt + 1); end
-//   end     
-   
-//     always_ff @(posedge i_clk)
-//         signal <= (i_rst) ? S0 : signal;   
-
 
 
     crc #(
@@ -137,7 +136,7 @@ module source#(
 		.i_crc_s_rst_p (s_valid && s_ready && signal == S5), // Sync Reset, Active High. Reset CRC To Initial Value.
 		.i_crc_ini_vld ('0     ), // Input Initial Valid
 		.i_crc_ini_dat ('0     ), // Input Initial Value
-		.i_crc_wrd_vld (s_valid && s_ready), // Word Data Valid Flag 
+		.i_crc_wrd_vld ((s_valid && s_ready && signal != S1) /*|| (s_valid && s_ready && signal == S2)*/), // Word Data Valid Flag 
         .o_crc_wrd_rdy (       ), // Ready To Recieve Word Data
 		.i_crc_wrd_dat (s_data ), // Word Data
 		.o_crc_res_vld (m_valid), // Output Flag of Validity, Active High for Each WORD_COUNT Number
