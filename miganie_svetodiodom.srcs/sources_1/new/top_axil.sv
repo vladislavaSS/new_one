d267dade1e0ddb1a108fa3fa76da701f8645e470
vladislavaSS <166354868+vladislavaSS@users.noreply.github.com>
2024-06-03 14:47:49 +0700

    Revert "lab5 axil"
    This reverts commit e6791f0b8219f22d2772122a56c5c65a776a0ec3.

--- miganie_svetodiodom.srcs/sources_1/new/top_axil.sv
+++ miganie_svetodiodom.srcs/sources_1/new/top_axil.sv
@@ -3,42 +3,26 @@
 module top_axil(
  input i_clk,
  input [2:0] i_rst,
- input i_rst_reg,
-  
- if_axil.s s_axil
+ 
+ if_axil.s s_axil;
 );
 
-logic o_error,        
-      o_err_mis_tlast,  
-      o_err_unx_tlast;
+ logic o_good,        
+       o_err_mis_tlast,  
+       o_err_unx_tlast;
 
 reg [7 : 0] o_len;
 
 (* keep_hierarchy="yes" *) 
-top_axi  TOP_AXI
-    (
+top_axi UUT_2 (
     .i_clk (i_clk),
     .i_rst (i_rst),
-    .i_lenght (o_len),
-    .o_error_top (o_error),        
-    .o_err_mis_tlast_top (o_err_mis_tlast),  
-    .o_err_unx_tlast_top (o_err_unx_tlast)
+    .i_length (o_len),
+
+    .o_err_crc (o_good),        
+    .o_err_mis_tlast (o_err_mis_tlast),  
+    .o_err_unx_tlast (o_err_unx_tlast)
     );
-
- (* keep_hierarchy="yes" *) 
- reg_map REG_MAP (
-    .i_error(o_error),        
-    .i_err_mis_tlast(o_err_mis_tlast),        
-    .i_err_unx_tlast(o_err_unx_tlast),
-    .i_clk(i_clk),
-    .i_rst(i_rst_reg),
-
-    .o_length(o_len),
-    .o_err(),
-
-    .s_axil(s_axil)
-
- );  
 
 
 
