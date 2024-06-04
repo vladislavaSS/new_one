938b820c5240d864a425bc44ff42f63af91267a4
vladislavaSS <166354868+vladislavaSS@users.noreply.github.com>
2024-06-04 12:36:16 +0700

    update top_axil
    в предыдущем коммите неполный файл

--- miganie_svetodiodom.srcs/sources_1/new/top_axil.sv
+++ miganie_svetodiodom.srcs/sources_1/new/top_axil.sv
@@ -3,26 +3,43 @@
 module top_axil(
  input i_clk,
  input [2:0] i_rst,
- 
- if_axil.s s_axil;
+ input i_rst_reg,
+  
+ if_axil.s s_axil
 );
 
- logic  o_good,        
-        o_err_mis_tlast,  
-        o_err_unx_tlast;
+logic o_error,        
+      o_err_mis_tlast,  
+      o_err_unx_tlast;
 
 reg [7 : 0] o_len;
 
 (* keep_hierarchy="yes" *) 
-top_axi UUT_2 (
+top_axi  TOP_AXI
+    (
     .i_clk (i_clk),
     .i_rst (i_rst),
-    .i_length (o_len),
+    .i_lenght (o_len),
+    .o_error_top (o_error),        
+    .o_err_mis_tlast_top (o_err_mis_tlast),  
+    .o_err_unx_tlast_top (o_err_unx_tlast)
 
-    .o_err_crc (o_good),        
-    .o_err_mis_tlast (o_err_mis_tlast),  
-    .o_err_unx_tlast (o_err_unx_tlast)
     );
+
+ (* keep_hierarchy="yes" *) 
+ reg_map REG_MAP (
+    .i_error(o_error),        
+    .i_err_mis_tlast(o_err_mis_tlast),        
+    .i_err_unx_tlast(o_err_unx_tlast),
+    .i_clk(i_clk),
+    .i_rst(i_rst_reg),
+
+    .o_length(o_len),
+    .o_err(),
+
+    .s_axil(s_axil)
+
+ );  
 
 
 
